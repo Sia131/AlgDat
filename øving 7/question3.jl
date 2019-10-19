621f6010755e241ef7c5abfe0eb7422d0e02ea2e
Siawash Naqibi <siawash@live.no>
2019-10-19 02:31:34 +0200

    done with 7

--- øving 7/question3.jl
+++ øving 7/question3.jl
@@ -4,25 +4,34 @@ function mincoins(coins, value)
     is_greedy = usegreed(coins)
     if is_greedy == true
         return mincoinsgreedy(coins, value)
+    else
+        memo = fill(Nothing, value + 1)
+        return recursion(coins, value, memo)
     end
 end
 
 function recursion(coins, value, memo)
-    temp = []
-    if value > 0
-        for i = 1:length(coins)
-            if coins[i] <= value
-                push!(temp, 1 + recursion(coins,value-coins[i]))
+    if memo[value + 1] == Nothing
+        if value > 0
+            temp = []
+            for i = 1:length(coins)
+                if coins[i] <= value
+                    push!(temp, 1 + recursion(coins,value-coins[i],memo))
+                end
             end
+            return memo[value + 1] = minimum(temp)
+        else
+            return memo[value + 1] = 0
         end
-        return minimum(temp)
     else
-        return 0
+        return memo[value + 1]
     end
 end
 
-coins = [4 2 1]
-value = 8
+coins = [1000 500 100 20 5 1]
+value = 1226
+memo = fill(0, value + 1)
+#print(memo)
 
-number_of_coins = recursion(coins, value)
+number_of_coins = recursion(coins, value,memo)
 print(number_of_coins)
